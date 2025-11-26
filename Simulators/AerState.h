315d867a2a975b521547469ac550820a581a2148
Adrian Roman <aroman_ro@yahoo.com>
2025-11-26 16:51:00 +0200

    qiskit aer limit is exclusive

--- Simulators/AerState.h
+++ Simulators/AerState.h
@@ -687,7 +687,7 @@ public:
 		  std::to_string(enableMultithreading ? 0 : 1); // 0 means auto/all available, 1 limits to 1
       state->configure("max_parallel_threads", nrThreads);
       state->configure("parallel_state_update", nrThreads);
-      const std::string threadsLimit = std::to_string(13);
+	  const std::string threadsLimit = std::to_string(12); // set one less, multithreading is started if the value is bigger than this
       state->configure("statevector_parallel_threshold", threadsLimit);
     }
   }
